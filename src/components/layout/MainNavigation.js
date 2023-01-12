1d5eb9be70f62e344da90f784b76c8000838b832
Jayanta Adhikary <73451350+jayantaadhikary@users.noreply.github.com>
2023-01-13 00:05:47 +0530

    Adding Favorites Page

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,9 +1,12 @@
-import React from "react";
+import { useContext } from "react";
 import { Link } from "react-router-dom";
 
 import classes from "./MainNavigation.module.css";
+import FavoritesContext from "../../store/favorites-context";
 
 function MainNavigation() {
+  const favoritesCtx = useContext(FavoritesContext);
+
   return (
     <div>
       <header className={classes.header}>
@@ -17,7 +20,12 @@ function MainNavigation() {
               <Link to="/new-meetup">Add Meetups</Link>
             </li>
             <li>
-              <Link to="/favorites">My Favorites</Link>
+              <Link to="/favorites">
+                My Favorites
+                <span className={classes.badge}>
+                  {favoritesCtx.totalFavorites}
+                </span>
+              </Link>
             </li>
           </ul>
         </nav>
